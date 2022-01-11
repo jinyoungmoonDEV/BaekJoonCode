0e302d0ca8e431abe5c1d73036fd4b944304e2b7
jinyoungmoonDEV <moonjinyoung.dev@gmail.com>
2022-01-11 14:15:26 +0900

    [Update]new code

---
+++ BaekJoon/src/Step1/num10430.java
@@ -0,0 +1,20 @@
+package Step1;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+public class num10430 {
+
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine()," ");
+		int A = Integer.parseInt(st.nextToken());
+		int B = Integer.parseInt(st.nextToken());
+		int C = Integer.parseInt(st.nextToken());
+		System.out.println( (A+B)%C );
+		System.out.println( (A%C + B%C)%C );
+		System.out.println( (A*B)%C );
+		System.out.println( (A%C * B%C)%C );
+	}
+
+}
---
+++ BaekJoon/src/Step1/num10869.java
@@ -0,0 +1,23 @@
+package Step1;
+import java.util.Scanner;
+import java.io.BufferedWriter;
+import java.io.OutputStreamWriter;
+import java.io.IOException;
+public class num10869 {
+
+	public static void main(String[] args) throws IOException{
+		Scanner in = new Scanner(System.in);
+		int A = in.nextInt();
+		int B = in.nextInt();
+		in.close();
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		bw.write((A+B) + "\n");
+		bw.write((A-B) + "\n");
+		bw.write((A*B) + "\n");
+		bw.write((A/B) + "\n");
+		bw.write((A%B) + "\n");
+		bw.flush();
+		bw.close();
+	}
+
+}
