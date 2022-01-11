ffa2fac3c895b0e5f95a4c04c5aed054bc4ff749
jinyoungmoonDEV <moonjinyoung.dev@gmail.com>
2022-01-11 10:46:20 +0900

    [Update]new product

---
+++ BaekJoon/src/Step1/num10718.java
@@ -0,0 +1,12 @@
+package Step1;
+
+public class num10718 {
+
+	public static void main(String[] args) {
+		StringBuffer sb = new StringBuffer();
+		sb.append("강한친구 대한육군\n");
+		sb.append("강한친구 대한육군");
+		System.out.print(sb);
+	}
+
+}
