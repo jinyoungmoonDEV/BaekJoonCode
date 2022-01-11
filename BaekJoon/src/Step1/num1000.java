3d36aec054b2fbbcd5dd342a57acb72043d4cda1
jinyoungmoonDEV <moonjinyoung.dev@gmail.com>
2022-01-11 13:16:51 +0900

    [Update]new code

--- BaekJoon/src/Step1/num1000.java
+++ BaekJoon/src/Step1/num1000.java
@@ -1,4 +1,5 @@
 package Step1;
+/*
 import java.util.Scanner;
 public class num1000 {
 
@@ -9,5 +10,21 @@ public class num1000 {
 		System.out.println(A+B);
 		in.close();
 	}
+}
+*/
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+public class num1000 {
 
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String str = br.readLine();
+		//or StringTokenizer st = new StringTokenizer(br.readLine()," ");
+		StringTokenizer st = new StringTokenizer(str," ");          
+		int a = Integer.parseInt(st.nextToken());
+		int b = Integer.parseInt(st.nextToken());
+		System.out.print(a+b);
+	}
 }
