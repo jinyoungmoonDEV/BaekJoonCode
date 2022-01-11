caa57a567fab3fa11e08131960905e7c6b1962d5
jinyoungmoonDEV <moonjinyoung.dev@gmail.com>
2022-01-11 10:03:21 +0900

    [Update] 2022-01-11

--- BaekJoon/src/Step1/num2557.java
+++ BaekJoon/src/Step1/num2557.java
@@ -3,8 +3,45 @@ package Step1;
 public class num2557 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.print("Hello World!");
+		//System.out.println("Hello World!");
+		//System.out.printf("Hello World!");
+		//System.out.printf("%s", Hello World!");
 	}
-
 }
+
+/*
+import java.io.BufferedWriter;
+import java.io.OutputStreamWriter;
+import java.io.IOException;
+
+public class num2557 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		bw.write("Hello World!");
+		bw.flush();
+		bw.close();
+	}
+}
+*/
+/*
+public class num2557 {
+
+	public static void main(String[] args) {
+		StringBuilder sb = new StringBuilder();
+		sb.append("Hello World!");
+		System.out.println(sb);
+	}
+}
+*/
+/*
+public class num2557 {
+
+	public static void main(String[] args) {
+		StringBuffer sb = new StringBuffer();
+		sb.append("Hello World!");
+		System.out.println(sb);
+	}
+}
+*/
