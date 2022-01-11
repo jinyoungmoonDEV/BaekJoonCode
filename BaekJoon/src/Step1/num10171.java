9af33fc5d3be79aae535502eaf2c4776a12fcb60
jinyoungmoonDEV <moonjinyoung.dev@gmail.com>
2022-01-11 10:58:07 +0900

    [Update]new code

---
+++ BaekJoon/src/Step1/num10171.java
@@ -0,0 +1,14 @@
+package Step1;
+
+public class num10171 {
+
+	public static void main(String[] args) {
+		StringBuffer sb = new StringBuffer();
+		sb.append("\\    /\\\n");
+		sb.append(" )  ( ')\n");
+		sb.append("(  /  )\n");
+ 		sb.append(" \\(__)|\n");
+ 		System.out.print(sb);
+	}
+
+}
