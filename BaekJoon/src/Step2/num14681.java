fb591c87d010c42e9daf52651e9190d0778ffe60
jinyoungmoonDEV <moonjinyoung.dev@gmail.com>
2022-01-12 12:19:35 +0900

    [Update]new code

---
+++ BaekJoon/src/Step2/num14681.java
@@ -0,0 +1,25 @@
+package Step2;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+public class num14681 {
+
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int x = Integer.parseInt(br.readLine());
+		int y = Integer.parseInt(br.readLine());
+		
+		if(x>0) {
+		if(y>0) System.out.println("1");
+		else System.out.println("4");
+		}
+		
+		else {
+			if(y>0) System.out.println("2");
+			else System.out.println("3");
+		}
+		
+	}
+
+}
