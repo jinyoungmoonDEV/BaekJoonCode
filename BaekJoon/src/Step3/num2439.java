23f3cb8ceb40eac500abf6d17781c31dc53ffccd
jinyoungmoonDEV <moonjinyoung.dev@gmail.com>
2022-01-13 10:31:40 +0900

    [Update]new code

--- BaekJoon/src/Step3/num2439.java
+++ BaekJoon/src/Step3/num2439.java
@@ -8,13 +8,14 @@ public class num2439 {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int N = Integer.parseInt(br.readLine());
 		
-		for(int i = 1; i < N; i++) {
-			System.out.print(" ");
+		for(int i = 1; i <= N; i++) {
+			for(int j = 1; j <= N-i; j++) {
+				System.out.print(" ");		
+			}
+			for(int k = 1; k <= i; k++) {
+				System.out.print("*");
+			}
+			System.out.print('\n');
 		}
-		for(int j = 1; j <= N-j; j++) {
-			System.out.print("*");
-		}
-			System.out.print('\n');	
 	}
-}	
-
+}
