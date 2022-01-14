657e17f244babc3896cdf347c77694c3a941d621
jinyoungmoonDEV <moonjinyoung.dev@gmail.com>
2022-01-14 11:44:15 +0900

    [Update]new code

---
+++ BaekJoon/src/Step5/num8958.java
@@ -0,0 +1,17 @@
+package Step5;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+public class num8958 {
+
+	public static void main(String[] args) {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		StringTokenizer st;
+		for(int i = 0; i < N; i++) {
+			
+		}
+	}
+
+}
