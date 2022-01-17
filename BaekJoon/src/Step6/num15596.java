6ef210b67505590cf0e08c710801cad8ff87b0ad
jinyoungmoonDEV <moonjinyoung.dev@gmail.com>
2022-01-17 11:31:33 +0900

    [Update]new code

---
+++ BaekJoon/src/Step6/num15596.java
@@ -0,0 +1,11 @@
+package Step6;
+
+public class num15596 {
+	long sum(int[] a) {
+		long sum = 0;
+		for(int i = 0; i < a.length; i++) {
+			sum+= a[i];
+		}
+		return sum;
+	}
+}
---
+++ BaekJoon/src/Step6/num4673.java
@@ -0,0 +1,8 @@
+package Step6;
+
+public class num4673 {
+
+	public static void main(String[] args) {
+	}
+
+}
