7c9012cbbd3798f12336a741b7084478c093e143
jinyoungmoonDEV <moonjinyoung.dev@gmail.com>
2022-01-26 14:48:48 +0900

    [Update]new code

--- BaekJoon/src/Step7/num11654.java
+++ BaekJoon/src/Step7/num11654.java
@@ -2,7 +2,10 @@ package Step7;
 
 public class num11654 {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception{
+		int a = System.in.read();
+		System.out.print(a);
+		
 	}
 
 }
